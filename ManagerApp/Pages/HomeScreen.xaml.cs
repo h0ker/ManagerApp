2446bd19067f71fd50e8899523ca9a83cfc16dc8
iotarex <z_series@tuta.io>
2020-04-15 20:15:40 -0700

    WIP for clocking in functionality

--- ManagerApp/Pages/HomeScreen.xaml.cs
+++ ManagerApp/Pages/HomeScreen.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ManagerApp.Models;
+using ManagerApp.Models.ServiceRequests;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,11 +19,9 @@ using Windows.UI.Xaml.Navigation;
 
 namespace ManagerApp.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class HomeScreen : Page
     {
+        public DispatcherTimer timer = new DispatcherTimer();
         public HomeScreen()
         {
             this.InitializeComponent();
@@ -31,6 +30,33 @@ namespace ManagerApp.Pages
             uxCouponButton.Click += UxCouponButton_Clicked;
             uxEmployeeButton.Click += UxEmployeeButton_Clicked;
             uxMenuEditButton.Click += UxMenuEditButton_Clicked;
+            uxEmployeeListView.ItemClick += UxEmployeeListView_ItemClick;
+
+            timer.Interval = TimeSpan.FromSeconds(1);  
+            timer.Tick += Timer_Tick;  
+            timer.Start();  
+
+            RefreshEmployeeList();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            secondHand.Angle = DateTime.Now.Second * 6;  
+            minuteHand.Angle = DateTime.Now.Minute * 6;  
+            hourHand.Angle = (DateTime.Now.Hour * 30);  
+        }
+
+        private void UxEmployeeListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            uxTimePopup.IsOpen = true;
+        }
+
+        public async void RefreshEmployeeList()
+        {
+            RealmManager.RemoveAll<EmployeeList>();
+            RealmManager.RemoveAll<Employee>();
+            await GetEmployeeListRequest.SendGetEmployeeListRequest();
+            uxEmployeeListView.ItemsSource = RealmManager.All<EmployeeList>().FirstOrDefault().employees.ToList();
         }
 
         private void UxMenuEditButton_Clicked(object sender, RoutedEventArgs e)
