c2ec4903a3b62836bf6e0788247396667b2f2380
iotarex <z_series@tuta.io>
2020-04-19 01:01:51 -0500

    Finished Tips and Comps

---
+++ ManagerApp/Models/ServiceRequests/GetEmployeeTips.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Models.ServiceRequests
+{
+    public class GetEmployeeTips : ServiceRequest
+    {
+        public override string Url { get; set; }
+        public override HttpMethod Method => HttpMethod.Get;
+
+        public GetEmployeeTips(string id)
+        {
+            Url = "https://dijkstras-steakhouse-restapi.herokuapp.com/tips/" + id;
+        }
+
+        public async static Task<bool> SendGetEmployeeTips(string id)
+        {
+            GetEmployeeTips getEmployeeTips = new GetEmployeeTips(id);
+            var response = await ServiceRequestHandler.MakeServiceCall<Tips>(getEmployeeTips);
+            if(response != null)
+            {
+                RealmManager.RemoveAll<Tips>();
+                RealmManager.RemoveAll<Tip>();
+                RealmManager.AddOrUpdate<Tips>(response);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ManagerApp/Models/Tip.cs
@@ -0,0 +1,18 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerApp.Models
+{
+    class Tip : RealmObject
+    {
+        public string _id { get; set; }
+        public string employee_id { get; set; }
+        public double tip_amount { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+    }
+}
